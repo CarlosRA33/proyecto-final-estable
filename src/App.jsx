8435ba8e37fef304cc8205e4406039d017dc5786
Carlos Rodriguez Arias <info.lernenit@gmail.com>
2025-08-16 22:13:29 -0500

    Añadida nueva funcionalidad X

--- src/App.jsx
+++ src/App.jsx
@@ -515,6 +515,9 @@ export default function App() {
   
   return (
     <div className="bg-gray-50 min-h-screen font-sans">
+      <header className="bg-white shadow p-4 text-xl font-bold text-gray-800">
+        Plataforma de seguimiento grupo INVENTIA
+      </header>
       <header className="bg-white shadow-md p-4 flex justify-between items-center">
         <div className="flex items-center">
             <svg className="h-8 w-8 text-blue-600 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" /></svg>
